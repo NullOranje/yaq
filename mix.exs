294031a2ff325085f47a1fe1b2b9e64beb762379
Nick McKinney <nick@ndmckinney.net>
2020-05-22 15:50:00 -0400

    Update package version to v1.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Yaq.MixProject do
   def project do
     [
       app: :yaq,
-      version: "1.1.0",
+      version: "1.2.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
